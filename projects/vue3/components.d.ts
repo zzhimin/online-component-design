edaf26f8ddb4387fbcf5a272723ee4798c52d10e
zzhimin <1002588003@qq.com>
2024-07-04 09:19:20 +0800

    🐳 chore: deploy.yml

--- projects/vue3/components.d.ts
+++ projects/vue3/components.d.ts
@@ -8,7 +8,6 @@ export {}
 declare module 'vue' {
   export interface GlobalComponents {
     AButton: typeof import('ant-design-vue/es')['Button']
-    ADrawer: typeof import('ant-design-vue/es')['Drawer']
     ALayout: typeof import('ant-design-vue/es')['Layout']
     ALayoutContent: typeof import('ant-design-vue/es')['LayoutContent']
     ALayoutHeader: typeof import('ant-design-vue/es')['LayoutHeader']
@@ -16,9 +15,6 @@ declare module 'vue' {
     ATabPane: typeof import('ant-design-vue/es')['TabPane']
     ATabs: typeof import('ant-design-vue/es')['Tabs']
     CodeEditor: typeof import('./src/components/CodeEditor.vue')['default']
-    DyamicComponent: typeof import('./src/components/DyamicComponent.vue')['default']
-    DynamicRender: typeof import('./src/components/DynamicRender.vue')['default']
-    HelloWorld: typeof import('./src/components/HelloWorld.vue')['default']
     IconCommunity: typeof import('./src/components/icons/IconCommunity.vue')['default']
     IconDocumentation: typeof import('./src/components/icons/IconDocumentation.vue')['default']
     IconEcosystem: typeof import('./src/components/icons/IconEcosystem.vue')['default']
@@ -26,8 +22,6 @@ declare module 'vue' {
     IconTooling: typeof import('./src/components/icons/IconTooling.vue')['default']
     RouterLink: typeof import('vue-router')['RouterLink']
     RouterView: typeof import('vue-router')['RouterView']
-    TheWelcome: typeof import('./src/components/TheWelcome.vue')['default']
-    WelcomeItem: typeof import('./src/components/WelcomeItem.vue')['default']
     Widget: typeof import('./src/components/widget/Widget.vue')['default']
   }
 }
--- projects/vue3/vite.config.js
+++ projects/vue3/vite.config.js
@@ -8,6 +8,10 @@ import { AntDesignVueResolver } from 'unplugin-vue-components/resolvers';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: 'vue3-online-component-design',
+  build: {
+    outDir: '../../dist'
+  },
   plugins: [
     vue(),
     vueDevTools(),
